46508b91f895a69a5dac595fb17068725669bdf5
CryptoBadBoy <raiman.elshan@gmail.com>
2019-10-13 17:49:23 +0300

    fix bug and add script

---
+++ test/deploy/updateNeutrinoAsset.develop.js
@@ -0,0 +1,34 @@
+const wvs = 10 ** 8;
+
+const symbolNeutrino = "BTC-N"
+const symbolBond = "BTC-NB"
+const descriptionNeutrino = "Bitcoin neutrino asset" 
+const descriptionBond = "Bitcoin neutrino bond asset" 
+
+const accounts = 
+    {
+        auctionContract: "",
+        neutrinoContract: "",
+        rpdContract: ""
+    }
+describe('Deploy', async function () {
+    before(async function () {
+       
+    });
+    it('Finalizing', async function () { 
+        const scriptNeutrino = compile(file('../neutrino.ride'));
+        const setScriptNeutrino = setScript({ script: scriptNeutrino, fee: 1400000 ,}, accounts.neutrinoContract);
+        await broadcast(setScriptNeutrino);
+        await waitForTx(setScriptNeutrino.id)
+
+        const scriptAuction = compile(file('../auction.ride'));
+        const setScriptAuctionTx = setScript({ script: scriptAuction, fee: 1400000,}, accounts.auctionContract);        
+        await broadcast(setScriptAuctionTx);
+        await waitForTx(setScriptAuctionTx.id)
+
+        const scriptRPD = compile(file('../rpd.ride'));
+        const setScriptRPDTx = setScript({ script: scriptRPD, fee: 1400000,}, accounts.rpdContract);        
+        await broadcast(setScriptRPDTx);
+        await waitForTx(setScriptRPDTx.id)
+    })
+})
