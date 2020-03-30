a1e86c665f381504c6232209345a0a814bc0aa66
Shamil Khalilov <shamfama@gmail.com>
2020-03-30 14:16:14 +0300

    Add upper bound param

--- test/sponsorship.test.js
+++ test/sponsorship.test.js
@@ -1,24 +1,27 @@
 const fs = require('fs');
 const axios = require('axios')
-const { sponsorship, broadcast, setScript } = require('@waves/waves-transactions');
+const { sponsorship, broadcast, setScript, issue } = require('@waves/waves-transactions');
 const { assert } = require('console');
 
 const nodeUrl = 'https://testnode1.wavesnodes.com';
 const WAVES_DECIMAL = 1e8;
 const chainId = 'T';
-const seed = 'foil'
+const seed = 'tone repeat unlock increase slush expect element same river pretty pizza axis elegant split tornado'
+
 
 describe('some suite', () => {
-    const desiredPrice = 93;
+    const desiredPrice = 91;
+    const upperBound = 75;
+    let assetId = 'HzWJjh9Fzqq6LkN2CnYcvTC2RqBW5rv2P6zPBDEDKnDr'
 
-    const checkComputedMinAssetFee = async () => {
-        const controlPrice = desiredPrice;
+    const checkComputedMinAssetFee = async (controlPrice) => {
         const increaseBy = (value, percent) => value + (percent / 100) * value;
 
         const sponsorData = {
-            assetId: '3yfJtVTEqsQUpNLGz4ztJmMgEsCCoyjCFx8VphKn3ZWc',
-            minSponsoredAssetFee: increaseBy(controlPrice, 75) * 10,
+            assetId: assetId,
+            minSponsoredAssetFee: increaseBy(controlPrice, upperBound) * 10,
             chainId: 'T',
+            fee: 100400000
         };
 
         const signedTx = sponsorship(sponsorData, seed);
@@ -27,9 +30,30 @@ describe('some suite', () => {
             const res = await broadcast(signedTx, nodeUrl);
             console.log(res)
         } catch (err) {
-            console.log(`Error: ${err.message}`);
+            console.log(`Error: ${err}`);
         }
     };
+
+    // it('create test asset', async () => {
+    //     const params = {
+    //         name: 'USDNTT',
+    //         description: 'Testnet Neutrino',
+    //         quantity: 1000000 * 1e6,
+    //         reissuable: false,
+    //         decimals: 6,
+    //         chainId: chainId,
+    //         fee: 100400000
+    //     }
+
+    //     const signedIssueTx = issue(params, seed)
+
+    //     broadcast(signedIssueTx, nodeUrl)
+    //         .then(res => {
+    //             console.log(res)
+    //             assetId = res.assetId
+    //         })
+    //         .catch(err => console.log(err))
+    // })
 
     it('load script piece', async () => {
         const scriptfile = fs.readFileSync('script/neutrino.ride');
@@ -70,7 +94,8 @@ describe('some suite', () => {
             console.log(`Error: ${err.message}`);
         }
 
-        await checkComputedMinAssetFee()
+        await checkComputedMinAssetFee(desiredPrice)
     });
+
     // it('check computed min asset fee', checkComputedMinAssetFee)
 });
