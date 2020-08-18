dffb32fc850e408bb8741997f3c83c485b51e9cf
Petr Kozhevnikov <pkozhevnikov@web3tech.ru>
2020-08-18 18:01:28 +0300

    Remove license comments

--- script_eth/contracts/Migrations.sol
+++ script_eth/contracts/Migrations.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
 contract Migrations {
--- script_eth/contracts/staking/usdn/Deprecatable.sol
+++ script_eth/contracts/staking/usdn/Deprecatable.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.0;
 
 import "./Ownable.sol";
